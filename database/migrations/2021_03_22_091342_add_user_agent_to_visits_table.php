d1c629ac26744628b074731826bb70028eacb6da
yevge <yevgeniy@wang>
2021-03-22 11:19:13 +0200

    Separate migrations for UserAgent

--- database/migrations/2021_03_22_091342_add_user_agent_to_visits_table.php
+++ database/migrations/2021_03_22_091342_add_user_agent_to_visits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVisitsTable extends Migration
+class AddUserAgentToVisitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateVisitsTable extends Migration
      */
     public function up()
     {
-        Schema::create('visits', function (Blueprint $table) {
-            $table->id();
-            $table->string('ip');
-            $table->string('continent_code');
-            $table->string('country_code');
-            $table->timestamps();
+        Schema::table('visits', function (Blueprint $table) {
+            $table->string('browser');
+            $table->string('os');
         });
     }
 
@@ -29,6 +26,8 @@ class CreateVisitsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('visits');
+        Schema::table('visits', function (Blueprint $table) {
+            $table->dropColumn(['browser', 'os']);
+        });
     }
 }
